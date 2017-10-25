66148c15f726a2869ce4ce9c9da32b7ead543a32
Varato <imxinchen@outlook.com>
2017-10-25 09:18:47 +0800

    add phasor2

--- insert.pyx
+++ insert.pyx
@@ -69,10 +69,9 @@ def insert(np.ndarray[np.float64_t, ndim = 2] slices, np.ndarray[np.float64_t, n
 
 
 # %%cython
-
 # import numpy as np 
 # cimport numpy as np 
-# from libc.math cimport sin, cos, sqrt, abs
+# from libc.math cimport sin, cos
 
 # def insert(np.ndarray[np.float64_t, ndim = 2] slices, np.ndarray[np.float64_t, ndim = 1] thetas):
 
@@ -80,7 +79,7 @@ def insert(np.ndarray[np.float64_t, ndim = 2] slices, np.ndarray[np.float64_t, n
 #     cdef np.ndarray[np.float64_t, ndim = 1] slc
 #     cdef int s0, s1, sc, c0, c1, x, y
 #     cdef double xx, yy, fx, fy, ifx, ify, w, th
-#     cdef Py_ssize_t i, j, k, t
+#     cdef Py_ssize_t i, t
 
 
 #     s0 = slices.shape[0]
@@ -94,11 +93,8 @@ def insert(np.ndarray[np.float64_t, ndim = 2] slices, np.ndarray[np.float64_t, n
 #     c1 = n1 // 2
 
 #     cdef np.ndarray[np.float64_t, ndim = 2] model = np.zeros((n0, n1), dtype = np.float64)
-#     cdef np.ndarray[np.float64_t, ndim = 1] values = np.zeros(s0*s1, dtype = np.float64)
-#     cdef np.ndarray[np.float64_t, ndim = 2] coords = np.zeros((s0*s1, 2))
-#     cdef np.ndarray[np.float64_t, ndim = 1] ds = np.zeros_like(values, dtype = np.float64)
-    
-#     k = 0
+#     cdef np.ndarray[np.float64_t, ndim = 2] weight = 2*np.ones((n0, n1), dtype = np.float64)
+
 #     for i, th in enumerate(thetas):
 #         slc = slices[i]
 #         for t, v in enumerate(slc):
@@ -110,30 +106,37 @@ def insert(np.ndarray[np.float64_t, ndim = 2] slices, np.ndarray[np.float64_t, n
 
 #             xx += c0
 #             yy += c1
-            
-#             values[k] = v
-#             coords[k, 0] = xx
-#             coords[k, 1] = yy
-#             k += 1
-            
-            
-#     for i in range(n0):
-#         print(i)
-#         for j in range(n1):
-#             dist = lambda coord: sqrt((i-coord[0])**2 + (j-coord[1])**2)
-#             ds = np.asarray(list(map(dist, coords)), dtype = np.float64)
-            
-#             indx = np.argsort(ds)[:4]
-#             nn_ds = np.where(ds[indx] == 0, 1e-7, ds[indx])
-#             nn_values = values[indx]
-#             weight = 1/nn_ds
-            
-#             model[i,j] = np.sum(weight * nn_values) / np.sum(weight)
-            
-#     return model
-            
-        
 
-            
+#             if xx <= 0 or xx >= n0 - 1: continue
+#             if yy <= 0 or yy >= n1 - 1: continue
 
+#             xxx = <int>xx
+#             yyy = <int>yy
 
+#             fx = xx - xxx
+#             fy = yy - yyy
+#             ifx = 1. - fx
+#             ify = 1. - fy
+
+#             d = np.sqrt(fx**2+fy**2)
+#             if d <1e-7: d=1e-7
+#             model[xxx, yyy] += v/d
+#             weight[xxx, yyy] += 1./d
+           
+#             d = np.sqrt(fx**2+(1-fy)**2)
+#             if d <1e-7: d=1e-7
+#             model[xxx, yyy + 1] += v/d
+#             weight[xxx, yyy + 1] += 1./d
+
+#             d = np.sqrt((1-fx)**2+(1-fy)**2)#fx * fy
+#             if d <1e-7: d=1e-7
+#             model[xxx + 1, yyy + 1] += v/d
+#             weight[xxx + 1, yyy + 1] += 1./d
+
+#             d = np.sqrt((1-fx)**2+fy**2) #fx * ify
+#             if d <1e-7: d=1e-7
+#             model[xxx + 1, yyy] += v/d
+#             weight[xxx + 1, yyy] += 1./d
+
+#     weight =  np.where(weight == 0., 1., weight)
+#     return model/weight
